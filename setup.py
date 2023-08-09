b193d7f8fa4470229061fd929d47e645f3cf2533
clsyzs-gyq <clsyzs-gyq@cas650.pi.sjtu.edu.cn>
2023-08-09 11:13:26 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1"
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="RGAST",
+    version=__version__,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    install_requires=[
+        'torch',
+        'scanpy',
+        'sklearn',
+        'torch_geometric',
+        'scipy',
+        'numba',
+        ],
+    author="Yuqiao Gong",
+    author_email="gyq123@sjtu.edu.cn",
+    keywords=["spatial transcriptomic", "RGAT", "representation learning", "spatial domain identification"],
+    description="Relational Graph Attention Network for Spatial Transcriptome Analysis",
+    license="MIT",
+    url='https://github.com/GYQ-form/RGAST',
+    long_description_content_type='text/markdown',
+    long_description=long_description
+)
